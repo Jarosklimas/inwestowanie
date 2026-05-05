1e56f318138b54eed06d6d8017b21c7ef8906efc
Jaroslaw Klimczyk <jaroslaw.klimczyk@globallogic.com>
2026-05-05 02:03:54 +0200

    Add send info telegram 2

--- scripts/gieldowy_bot_analityczny.py
+++ scripts/gieldowy_bot_analityczny.py
@@ -152,4 +152,4 @@ if __name__ == "__main__":
         print("✅ Analiza zakończona. Raport wysłany na Telegram!")
     else:
         print("⚪ Rynek stabilny, brak mocnych sygnałów.")
-        wyslij_telegram(wiadomosc_telegram)
+        wyslij_telegram("Test GitHuba: Rynek stabilny, bot działa, ale brak mocnych sygnałów dzisiaj.")
